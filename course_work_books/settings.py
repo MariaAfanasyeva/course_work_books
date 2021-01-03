b7b678373d7244d6f0de42e60cb9e82fa5dbba12
MariaAfanasyeva <masha.afanaseva.29@mail.ru>
2021-01-04 00:39:27 +0300

    first attempt

--- course_work_books/settings.py
+++ course_work_books/settings.py
@@ -128,9 +128,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_HOST_USER = 'masha.afanaseva.29@mail.ru'
-EMAIL_HOST_PASSWORD = 'mashanya292001'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'mahulyaafanasjeva@yandex.ru'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
